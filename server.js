561fa2810adbb1ac276586fc72819c1bb5263b38
Segun Konibire <gunzalez@hotmail.com>
2016-03-26 22:39:42 +0000

    UPDATE/ now uses sequelize

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ var db = require('./db.js');
 
 app.use(bodyParser.json());
 
-// list of todos
-//var todos = [{
-//	id: 1,
-//	description: "Learn Node.js",
-//	completed: false
-//}, {
-//	id: 2,
-//	description: "Scratch ass",
-//	completed: false
-//}, {
-//	id: 3,
-//	description: "Watch new movie",
-//	completed: true
-//}]
-
 // add a todo
 app.post('/todos', function(req, res){
 
@@ -50,11 +35,10 @@ app.get('/todos/:id', function(req, res){
     }).catch(function(error){
         res.status(500).json(error);
     });
-
 });
 
 
-// get all todos, search todos
+// get all todos, allows search on completed and q
 app.get('/todos', function(req, res){
 
     var query = req.query,
@@ -77,11 +61,10 @@ app.get('/todos', function(req, res){
     }).catch(function (error) {
         res.status(500).json(error);
     });
-
 });
 
 
-// delete a todo item
+// delete a todo
 app.delete('/todos/:id', function(req, res){
 
 	var todoId = parseInt(req.params.id);
@@ -95,42 +78,38 @@ app.delete('/todos/:id', function(req, res){
     }).catch(function(error){
         res.status(500).json(error);
     });
-	
 });
 
-// ============================================================
 
-// api - updates one todo item, model
+// update a todo
 app.put('/todos/:id', function(req, res){
 
-	var body = req.body;
-	var todo = _.pick(body, 'description', 'completed');
-	var validAttributes = {};
-	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+    var todoId = parseInt(req.params.id);
+	var attributes = {};
 
-	if(!matchedTodo){
-		return res.status(404).json({"error":"No Todo found with id: " + todoId});
+	if(body.hasOwnProperty('completed')){
+		attributes.completed = body.completed;
 	}
 
-	if(todo.hasOwnProperty('completed') && _.isBoolean(todo.completed)){
-		validAttributes.completed = todo.completed;
-	} else if (todo.hasOwnProperty('completed')) {
-		return res.status(400).send();
+	if(body.hasOwnProperty('description')){
+		attributes.description = body.description
 	}
 
-	if(todo.hasOwnProperty('description') && _.isString(todo.description) && todo.description.trim().length > 0){
-		validAttributes.description = todo.description
-	} else if(todo.hasOwnProperty('description')){
-		return res.status(400).send();
-	}
-
-	_.extend(matchedTodo, validAttributes);
- 	res.json(matchedTodo);
-
+    db.todo.findById(todoId).then(function(todo){
+        if(todo){
+            todo.update(attributes).then(function(todo){
+                res.json(todo.toJSON());
+            }, function(error){
+                res.status(400).json(error);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function(){
+        res.status(500).send();
+    });
 });
-
-// ============================================================
 
 // just checking the root works
 app.get('/', function(req, res){
